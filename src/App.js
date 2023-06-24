15b619c1dc81f08bb91a0f1c8d0a2a7eead69f44
IngDiiego <diipeza@gmail.com>
2023-06-23 20:19:47 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import imagen1 from '../src/img/MicrosoftTeams-image.png'
+import { useState } from 'react';
+import './App.css';
+import Characters from './components/Characters';
+
+function App() {
+  const[characters, setCharacters] = useState(null);
+  const reqApi = async () =>{
+    const api = await fetch ("https://rickandmortyapi.com/api/character")
+    //console.log(api)
+    //console.log("Clicking");
+    const characterApi = await api.json();
+    //console.log(characterApi)
+    //console.log(characters);
+    setCharacters(characterApi.results);
+    console.log(characters);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Rick & Morty</h1>
+        {characters ?(
+        <Characters characters={characters}/>
+        ):(
+          <>
+        <img src={imagen1} alt='Rick & Morty' className='img-home' />
+        <button onClick={reqApi} className='btn-search'>
+          Buscar Personajes
+        </button>
+        </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
